db867c7e880f139a7e916f8e59056e39f677f787
Michael Loomis <85528807+labordude@users.noreply.github.com>
2023-06-06 13:43:55 -0500

    updates

--- lib/main.py
+++ lib/main.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 from textual.app import App, ComposeResult, RenderResult
-from textual.events import Key
-from textual.containers import Container, Vertical, Horizontal
+from textual.events import Key, Event
+from textual.containers import (
+    Container,
+    Vertical,
+    Horizontal,
+    ScrollableContainer,
+    VerticalScroll,
+)
 from textual.widgets import (
     Button,
     Header,
@@ -11,12 +17,20 @@ from textual.widgets import (
     ListItem,
     Input,
     Label,
+    DataTable,
 )
-from textual.reactive import reactive
+from itertools import cycle
+from textual.reactive import reactive, Reactive
 from textual.strip import Strip
 from textual.screen import Screen
+from rich.align import Align
+from rich.box import DOUBLE
 from rich.segment import Segment
+from rich.panel import Panel
+from rich.console import RenderableType, group
 from rich.style import Style
+from rich.table import Table
+from rich.text import Text
 from textual.widget import Widget
 from models import Driver, Destination, Customer, Package, Status
 from sqlalchemy import create_engine, select, update
@@ -34,6 +48,8 @@ from helpers import (
     packages_by_customer,
     packages_by_destination,
     all_drivers,
+    all_customers,
+    all_destinations,
 )
 
 
@@ -47,42 +63,128 @@ class Menu(Static):
     def compose(self) -> ComposeResult:
         yield ListView(
             ListItem(Label("My Packages", classes="menu_item")),
-            ListItem(Label("", classes="menu_item")),
             ListItem(Label("Packages", classes="menu_item")),
             ListItem(Label("Customers", classes="menu_item")),
             ListItem(Label("Destinations", classes="menu_item")),
-            ListItem(Label("", classes="menu_item")),
             ListItem(Label("Search", classes="menu_item")),
         )
 
 
 class Content(Widget):
     def compose(self) -> ComposeResult:
-        yield Static("hello")
+        with VerticalScroll():
+            yield ShowDrivers()
+            yield ShowPackages()
+        # yield ShowCustomers()
+        # yield ShowDestinations()
 
-    pass
+
+cursors = cycle(["column", "row", "cell"])
+
+
+class ShowDrivers(Widget):
+    def compose(self) -> ComposeResult:
+        yield DataTable(id="drivers")
+
+    def on_mount(self) -> None:
+        drivers = [driver for driver in all_drivers()]
+
+        table = self.query_one("#drivers", DataTable)
+        table.cursor_type = next(cursors)
+        table.zebra_stripes = True
+        table.add_columns("id", "name")
+        for driver in drivers:
+            table.add_row(driver.id, driver.name)
+
+    def key_c(self):
+        table = self.query_one("#drivers", DataTable)
+        table.cursor_type = next(cursors)
+
+
+class ShowPackages(Widget):
+    def compose(self) -> ComposeResult:
+        yield DataTable(id="packages")
+
+    def on_mount(self) -> None:
+        packages = [package for package in all_packages()]
+
+        table = self.query_one("#packages", DataTable)
+        table.cursor_type = next(cursors)
+        table.zebra_stripes = True
+        table.add_columns("id", "customer", "destination")
+        for package in packages:
+            table.add_row(
+                package.id, package.customer.name, package.destination.name
+            )
+
+    def key_c(self):
+        table = self.query_one("#packages", DataTable)
+        table.cursor_type = next(cursors)
+
+
+class ShowCustomers(Widget):
+    def compose(self) -> ComposeResult:
+        yield DataTable(id="customers")
+
+    def on_mount(self) -> None:
+        customers = [customer for customer in all_customers()]
+
+        table = self.query_one("#customers", DataTable)
+        table.cursor_type = next(cursors)
+        table.zebra_stripes = True
+        table.add_columns("id", "name", "address")
+        for customer in customers:
+            table.add_row(customer.id, customer.name, customer.address)
+
+    def key_c(self):
+        table = self.query_one("#customers", DataTable)
+        table.cursor_type = next(cursors)
+
+
+class ShowDestinations(Widget):
+    def compose(self) -> ComposeResult:
+        yield DataTable(id="destinations")
+
+    def on_mount(self) -> None:
+        destinations = [destination for destination in all_destinations()]
+
+        table = self.query_one("#destinations", DataTable)
+        table.cursor_type = next(cursors)
+        table.zebra_stripes = True
+        table.add_columns("id", "name", "address")
+        for destination in destinations:
+            table.add_row(
+                destination.id, destination.name, destination.address
+            )
+
+    def key_c(self):
+        table = self.query_one("#destinations", DataTable)
+        table.cursor_type = next(cursors)
 
 
 class TrackyMcPackage(App):
-    CSS_PATH = "styles.css"
-    TITLE = "Tracky McPackage"
-    SUB_TITLE = "We'll get it there...eventually"
-    current_driver = reactive(4)
+    # async def on_load(self) -> None:
+    #     await self.bind("ctrl+c", "quit", "Quit")
 
-    def compose(self) -> ComposeResult:
-        yield Header()
-        yield Footer()
-        yield Menu(classes="box", id="sidebar")
+    engine = create_engine("sqlite:///fpds.db")
+    with Session(engine) as session:
+        CSS_PATH = "styles.css"
+        TITLE = "Tracky McPackage"
+        SUB_TITLE = "We'll get it there...eventually"
+        current_driver = reactive(4)
 
-        yield Content(classes="box", id="content")
+        def compose(self) -> ComposeResult:
+            yield Header()
+            yield Footer()
+            yield Menu(classes="box", id="sidebar")
+            yield Content(classes="box", id="content")
 
-    def on_key(self, event: Key) -> None:
-        self.exit()
+        # def on_key(self, event: Key) -> None:
+        #     self.exit()
 
 
 if __name__ == "__main__":
-    app = TrackyMcPackage()
-    app.run()
-    # drivers = all_drivers()
-    # for driver in drivers:
-    #     print(driver.name)
+    engine = create_engine("sqlite:///fpds.db")
+    with Session(engine) as session:
+        app = TrackyMcPackage()
+        app.run()
